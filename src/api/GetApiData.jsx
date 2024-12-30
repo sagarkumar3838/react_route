3473e2aa9717f08db441bc846daf178cd6293ac9
sagarkumar3838 <sagarsamal3838@gmail.com>
2024-12-30 23:51:38 +0530

    tenth commit

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -2,7 +2,7 @@ export const GetApiData = async()=>{
 
     try {
        const response = await fetch(
-           `http://www.omdbapi.com/?s=naruto&apikey=${import.meta.env.VITE_API_KEY}`
+           `https://www.omdbapi.com/?s=naruto&apikey=${import.meta.env.VITE_API_KEY}`
         )
         
 
